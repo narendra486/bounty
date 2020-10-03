218c778820d0ea616edab061d9c588961b36a934
narendra486 <pallanarendra486@gmail.com>
2020-10-03 12:01:37 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,13 @@
+#go Tools
+go get -u github.com/tomnomnom/anew
+go get -u github.com/tomnomnom/qsreplace
+go get -u github.com/jaeles-project/gospider
+go get github.com/hakluke/hakrawler
+go get -u github.com/tomnomnom/gf
+go get -u github.com/ndelphit/apkurlgrep
+go get -u github.com/ffuf/ffuf
+GO111MODULE=on go get -u github.com/projectdiscovery/chaos-client/cmd/chaos
+GO111MODULE=auto go get -u -v github.com/projectdiscovery/httpx/cmd/httpx
+GO111MODULE=on go get -u -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder
+
+
