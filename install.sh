c1c8436f1c08b42cc478bf17918b04c07b86c311
narendra486 <pallanarendra486@gmail.com>
2020-10-03 12:34:06 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#go Tools
+# Bugbounty go Tools list
 go get -u github.com/tomnomnom/anew
 go get -u github.com/tomnomnom/qsreplace
 go get -u github.com/jaeles-project/gospider
@@ -6,8 +6,13 @@ go get github.com/hakluke/hakrawler
 go get -u github.com/tomnomnom/gf
 go get -u github.com/ndelphit/apkurlgrep
 go get -u github.com/ffuf/ffuf
+go get -u github.com/tomnomnom/httprobe
 GO111MODULE=on go get -u github.com/projectdiscovery/chaos-client/cmd/chaos
 GO111MODULE=auto go get -u -v github.com/projectdiscovery/httpx/cmd/httpx
 GO111MODULE=on go get -u -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder
+
+
+# apt install
+sudo apt install amass
 
 
